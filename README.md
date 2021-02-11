7df76b3b6bc5816c2af384e999145897bcb3500a
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-02-11 22:12:18 +0100

    changed the reasme file

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-#Problem01
-The provided code reads an integer, n and for all non-negative integers i<n, prints i*i
+This repository provides some useful python solutions for different problems.
--- problem01.py
+++ problem01.py
@@ -1,7 +1,6 @@
 # Problem01:
 # The provided code reads an integer, n and for all non-negative integers i<n, prints i*i
 # The code starts here:
-
 i = 0
 n = int(input())
 for i in range (0, n):

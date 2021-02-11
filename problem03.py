e074356111f0e2253681b77502e94179f5938c0f
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-02-12 00:37:48 +0100

    problem 03 code has been modified

--- problem03.py
+++ problem03.py
@@ -1,6 +1,8 @@
 # problem03:
 # You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
-# Hello firstname lastname! You just delved into python
+# Hello firstname lastname! You just delved into python.
 # The code starts here:
 
-
+a = str (input())
+b = str (input())
+print ('Hello',a,b,'!','You just delved into python.')

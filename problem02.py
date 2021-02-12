bf919041a55b0f8b89f5991f5c93879af253d50e
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-02-12 01:11:04 +0100

    modified problem 02 code

--- problem02.py
+++ problem02.py
@@ -2,3 +2,16 @@
 # Given a year, determine whether it is a leap year. 
 # If it is a leap year, return the Boolean True, otherwise return False.
 # The code starts here:
+
+# Defining the function
+def leap_year ():
+ if year % 4 == 0:
+  print ('True')
+ else:
+  print ('False')
+
+# Calling the function
+year = int (input())
+leap_year ()
+
+

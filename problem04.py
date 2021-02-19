4159c001034f996d2f07af5341b49c20bc3ed05f
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-02-19 19:22:22 +0100

    new problem generated

---
+++ problem04.py
@@ -0,0 +1,6 @@
+# problem04:
+# The included code stub will read an integer n from the user
+# Without using any string methods, try to print the following: 123....n
+# Note that....represents the consecutive values in between.
+# The code starts here:
+

faa9b2b88dc6e6cbac3e60c0c5798143c102a964
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-02-19 23:56:02 +0100

    problem05 has been created

---
+++ problem05.py
@@ -0,0 +1,7 @@
+# problem05:
+# The first line contains the integer n, the number of student's records
+# The next  lines contain the names and marks obtained by a student
+# Each value separated by a space
+# The final line contains query_name, the name of a student to query
+# Print the average of the marks array for the student name provided, showing 2 places after the decimal
+# The code starts here:
